8bbbcce5ebac507353e70be9898118d68e57eccd
Anne Summers <ukulanne@gmail.com>
2019-03-04 18:12:10 -0600

    Update git-klone-setup.scm

--- git-klone-setup.scm
+++ git-klone-setup.scm
@@ -45,7 +45,7 @@
 Usage: git-klone-setup.scm [options]
 Clones a module from github and creates a backup of current version
   -m,  --module    Module to clone.
-  -o,  --org       Organization to use (aixtools default)
+  -o,  --org       Organization to use (ukulanne default)
   -b,  --branch	   Branch to use.
   -t,  --tag       Tag to use for backup directory
   -v,  --version   Display version.
